b2713e62350adb9f2b20394ee7730e74b5ece37e
Harshavardhana V <harshavardhana4nov@gmail.com>
2026-09-25 23:51:54 +0530

    Update ResultTest.java

--- src/test/java/dvprg1/ResultTest.java
+++ src/test/java/dvprg1/ResultTest.java
@@ -3,17 +3,17 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 public class ResultTest {
 	@Test
-	//function to check for passing marks
+	
 	public void testPass() {
 		Assert.assertEquals(Result.display(55),"pass");
 	}
 	@Test
-	//function to check for fail marks
+
 	public void testFail() {
 		Assert.assertEquals(Result.display(35),"fail");
 	}
 	@Test
-	//function to check for invalid input
+	
 	public void testInvalid() {
 		Assert.assertEquals(Result.display(-5),"invalid");
 	}
